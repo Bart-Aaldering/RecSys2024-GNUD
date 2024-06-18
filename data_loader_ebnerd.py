6f65c66abe85f60fdd1164254c7dec26ce51319f
Bart <ba.aaldering@gmail.com>
2024-06-18 14:16:56 +0200

    Changed it so we use main now

--- data_loader_ebnerd.py
+++ data_loader_ebnerd.py
@@ -109,7 +109,7 @@ def create_graph(json_history, len_news):
     return user_news, news_user
 
 
-def main(args):
+def load_data(args):
     PATH = Path("Data/ebnerd_demo")
     # ARTICLES_PATH = Path("Data/ebnerd_demo")
     data_split = "train"
@@ -262,42 +262,3 @@ def main(args):
     test_user_news, test_news_user = create_graph(test_history, len_news)
 
     return train_data, eval_data, test_data, train_user_news, train_news_user, test_user_news, test_news_user, news_title, news_entity, news_group
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--news_neighbor", type=int, default=30, help="the number of neighbors to be sampled")
-parser.add_argument("--entity_neighbor", type=int, default=40, help="the number of neighbors to be sampled")
-parser.add_argument("--user_neighbor", type=int, default=30, help="the number of neighbors to be sampled")
-parser.add_argument("--title_len", type=int, default=10, help="the max length of title")
-parser.add_argument("--ratio", type=float, default=0.2, help="the ratio of train data")
-parser.add_argument('--dataset', type=str, default='ten_week', help='which dataset to use')
-parser.add_argument('--session_len', type=int, default=10, help='the max length of session')
-parser.add_argument('--aggregator', type=str, default='neighbor', help='which aggregator to use')
-parser.add_argument('--n_epochs', type=int, default=5, help='the number of epochs')
-parser.add_argument('--user_dim', type=int, default=128, help='dimension of user and entity embeddings')
-parser.add_argument('--cnn_out_size', type=int, default=128, help='dimension of cnn output')
-parser.add_argument('--n_iter', type=int, default=2, help='number of iterations when computing entity representation')
-parser.add_argument('--batch_size', type=int, default=32, help='batch size')
-parser.add_argument('--l2_weight', type=float, default=5e-3, help='weight of l2 regularization')
-parser.add_argument('--lr', type=float, default=0.0005, help='learning rate')  #3e-4
-parser.add_argument('--save_path', type=str, default="./Data/1week/hop2/version1/", help='model save path')
-parser.add_argument('--test', type=int, default=0, help='test')
-parser.add_argument('--use_group', type=int, default=1, help='whether use group')
-parser.add_argument('--n_filters', type=int, default=64, help='number of filters for each size in KCNN')
-parser.add_argument('--filter_sizes', type=int, default=[2, 3], nargs='+',
-                    help='list of filter sizes, e.g., --filter_sizes 2 3')
-parser.add_argument('--ncaps', type=int, default=7,
-                    help='Maximum number of capsules per layer.')
-parser.add_argument('--dcaps', type=int, default=0, help='Decrease this number of capsules per layer.')
-parser.add_argument('--nhidden', type=int, default=16,
-                        help='Number of hidden units per capsule.')
-parser.add_argument('--routit', type=int, default=7,
-                    help='Number of iterations when routing.')
-parser.add_argument('--balance', type=float, default=0.004, help='learning rate')  #3e-4
-parser.add_argument('--version', type=int, default=0,
-                        help='Different version under the same set')
-
-args = parser.parse_args()
-
-data = main(args)
-train(args, data, True)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 import numpy as np
 import time
-from data_loader import load_data, load_new_data
+from data_loader_ebnerd import load_data
 from train import train
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
@@ -11,21 +11,22 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument("--news_neighbor", type=int, default=30, help="the number of neighbors to be sampled")
+parser.add_argument("--entity_neighbor", type=int, default=40, help="the number of neighbors to be sampled")
+parser.add_argument("--user_neighbor", type=int, default=30, help="the number of neighbors to be sampled")
+parser.add_argument("--title_len", type=int, default=10, help="the max length of title")
+parser.add_argument("--ratio", type=float, default=0.2, help="the ratio of train data")
 parser.add_argument('--dataset', type=str, default='ten_week', help='which dataset to use')
-parser.add_argument('--title_len', type=int, default=10, help='the max length of title')
 parser.add_argument('--session_len', type=int, default=10, help='the max length of session')
 parser.add_argument('--aggregator', type=str, default='neighbor', help='which aggregator to use')
-parser.add_argument('--n_epochs', type=int, default=1, help='the number of epochs')
-parser.add_argument('--user_neighbor', type=int, default=30, help='the number of neighbors to be sampled')
-parser.add_argument('--news_neighbor', type=int, default=10, help='the number of neighbors to be sampled')
-parser.add_argument('--entity_neighbor', type=int, default=1, help='the number of neighbors to be sampled')
+parser.add_argument('--n_epochs', type=int, default=5, help='the number of epochs')
 parser.add_argument('--user_dim', type=int, default=128, help='dimension of user and entity embeddings')
 parser.add_argument('--cnn_out_size', type=int, default=128, help='dimension of cnn output')
 parser.add_argument('--n_iter', type=int, default=2, help='number of iterations when computing entity representation')
-parser.add_argument('--batch_size', type=int, default=128, help='batch size')
+parser.add_argument('--batch_size', type=int, default=32, help='batch size')
 parser.add_argument('--l2_weight', type=float, default=5e-3, help='weight of l2 regularization')
 parser.add_argument('--lr', type=float, default=0.0005, help='learning rate')  #3e-4
-parser.add_argument('--save_path', type=str, default="./data/1week/hop2/version1/", help='model save path')
+parser.add_argument('--save_path', type=str, default="./Data/1week/hop2/version1/", help='model save path')
 parser.add_argument('--test', type=int, default=0, help='test')
 parser.add_argument('--use_group', type=int, default=1, help='whether use group')
 parser.add_argument('--n_filters', type=int, default=64, help='number of filters for each size in KCNN')
@@ -43,14 +44,13 @@ parser.add_argument('--version', type=int, default=0,
                         help='Different version under the same set')
 
 show_loss = True
-show_time = False
+show_time = True
 
-#t = time()
+t = time()
 
 args = parser.parse_args()
-# data = load_data(args)
-data = load_new_data(args)
+data = load_data(args)
 train(args, data, show_loss)
 
-#if show_time:
-#    print('time used: %d s' % (time() - t))
+if show_time:
+   print('time used: %d s' % (time() - t))
